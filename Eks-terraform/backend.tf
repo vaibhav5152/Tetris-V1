b7383779025c90a562313d4ff7f8c8ac0d67adcb
vaibhav5152 <129995960+vaibhav5152@users.noreply.github.com>
2024-07-18 13:01:36 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "argo-cd-project-backend01" # Replace with your actual S3 bucket name
-    key    = "terraform.tfstate"
+    key    = "jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
 }
